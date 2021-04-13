ec5494f4c9cfc4bfe9f115b472a9977847f3286b
Anil Pujari <pujarianil4@gmail.com>
2021-04-14 01:07:15 +0530

    feature section added

--- src/Library/Custom_UI.jsx
+++ src/Library/Custom_UI.jsx
@@ -9,5 +9,13 @@ font-size:20px;
 font-weight:600;
 color:white;
 border:0px;
-background-color: ${prop=>prop.color}
+background-color: ${prop=>prop.color};
+`
+
+export const Feature_card= styled.div`
+width:220px;
+height:300px;
+border-radius:10px;
+background-color:${prop=>prop.color};
+color: white;
 `
