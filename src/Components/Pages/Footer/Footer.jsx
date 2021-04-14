f1111c21c10533b91ea49408ed6da3f810396313
Anil Pujari <pujarianil4@gmail.com>
2021-04-14 16:29:42 +0530

    footer init

--- src/Components/Pages/Footer/Footer.jsx
+++ src/Components/Pages/Footer/Footer.jsx
@@ -1,15 +1,21 @@
 import React from "react"
+import { Link } from "react-router-dom"
 import style from "./Footer.module.css"
 export function Footer(){
 
     return(
+        <div className={style.Footer}>
         <div className={style.footer} >
             <div className={style.sitemap}>
-              <div className={style.harvest}></div>
+              <div className={style.harvest}>
+                  <h4>Harvest</h4>
+                  <Link></Link>
+              </div>
             </div>
             <div className={style.copyright}>
 
             </div>
+        </div>
         </div>
     )
 }
