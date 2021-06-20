eb7a47767b0813029746f7a138498cf762107137
kamal <kamaldro97@gmail.com>
2021-06-20 16:19:58 +0530

    added iframe video

--- src/Components/VideoTutorial/VideoTutorial.jsx
+++ src/Components/VideoTutorial/VideoTutorial.jsx
@@ -1,16 +1,7 @@
 import React from 'react'
 import styles from './VideoTutorial.module.css'
 
-import {
-    Player,
-    ControlBar,
-    ReplayControl,
-    ForwardControl,
-    CurrentTimeDisplay,
-    TimeDivider,
-    PlaybackRateMenuButton,
-    VolumeMenuButton
-  } from 'video-react';
+
 
 
 export const VideoTutorial = () => {
@@ -18,19 +9,9 @@ export const VideoTutorial = () => {
         <div className={styles.videowrapper}>
           
        <div className={styles.video}>
-       <Player src="https://masai-course.s3.ap-south-1.amazonaws.com/users/768/submissions/84078/218390/05408a678699dd875605a25b8f9780b7/Getharvest_Demo.mp4" poster="https://play-lh.googleusercontent.com/4zGNujuyCLpCWTTx_L7PmxVatkmStSUEx5L2GVD59zGyZ5mleKI_BRrB93I60rtNrDk" className={styles.players} >
-        
        
-  
-        <ControlBar>
-          <ReplayControl seconds={10} order={1.1} />
-          <ForwardControl seconds={30} order={1.2} />
-          <CurrentTimeDisplay order={4.1} />
-          <TimeDivider order={4.2} />
-          <PlaybackRateMenuButton rates={[5, 2, 1, 0.5, 0.1]} order={7.1} />
-          <VolumeMenuButton disabled />
-        </ControlBar>
-      </Player>
+
+      <iframe src="https://masai-course.s3.ap-south-1.amazonaws.com/users/768/submissions/84078/218390/05408a678699dd875605a25b8f9780b7/Getharvest_Demo.mp4" poster="https://play-lh.googleusercontent.com/4zGNujuyCLpCWTTx_L7PmxVatkmStSUEx5L2GVD59zGyZ5mleKI_BRrB93I60rtNrDk" frameborder="0" className={styles.players}></iframe>
        </div>
   
           
